ea6dab9350d62d488b61d5e38f4f750f7fbd4e62
melin <libinsong1204@gmail.com>
2013-09-26 19:24:16 +0800

    增加ConfigurationListener测试

--- super-diamond-client/src/test/java/com/github/diamond/client/PropertiesConfigurationFactoryBeanTest.java
+++ super-diamond-client/src/test/java/com/github/diamond/client/PropertiesConfigurationFactoryBeanTest.java
@@ -3,8 +3,13 @@
  */    
 package com.github.diamond.client;
 
+import java.io.IOException;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.github.diamond.client.event.ConfigurationEvent;
+import com.github.diamond.client.event.ConfigurationListener;
 
 
 /**
@@ -12,11 +17,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author bsli@ustcinfo.com
  */
 public class PropertiesConfigurationFactoryBeanTest {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
 		
 		PropertiesConfiguration configuration = PropertiesConfigurationFactoryBean.getPropertiesConfiguration();
+		configuration.addConfigurationListener(new ConfigurationListener() {
+	        @Override
+	        public void configurationChanged(ConfigurationEvent event) {
+	                            //监听配置参数变化,监听不到?
+	            System.out.println(event.getType().name() + " " + event.getPropertyName() + " " + event.getPropertyValue());
+	        }
+	    });
+		
+		System.in.read();
 	}
 
 }
